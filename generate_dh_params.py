b2b6bff061a9bc2aff611243978257811d9cea73
jGuilhermexe <jeangui.arruda@gmail.com>
2025-08-04 13:30:38 -0300

    Adição de alguns comentários e alguns usuários que foram cadastrados pra teste

--- generate_dh_params.py
+++ generate_dh_params.py
@@ -1,10 +1,17 @@
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives import serialization
 
-# Gera parâmetros DH
-parameters = dh.generate_parameters(generator=2, key_size=512)  # pode usar 1024 ou 2048 se quiser mais seguro
 
-# Salva em arquivo PEM
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GENERATE_DH_PARAMS.PY ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Esse arquivo foi responsável por gerar a dh_params.pem, que é crucial pra que o algoritmo Diffie-Hellman (DH)
+# funcione com sucesso, como o DH necessita de dois números grandes que são usados pelos clientes para gerar as 
+# suas chaves efêmeras, acaba que fica um peso um pouco maior, a dh_params.pem serve pra evitar uma carga a mais.
+
+
+# Responsável por gerar os parâmetros Diffie Hellman
+parameters = dh.generate_parameters(generator=2, key_size=512)
+
+# Salva no arquivo PEM
 with open("dh_params.pem", "wb") as f:
     f.write(parameters.parameter_bytes(
         encoding=serialization.Encoding.PEM,
